f1f526caa2f58ae8b67c13d2af841360fcc490e0
Swati <pandeyswati1043@gmail.com>
2026-06-08 23:17:17 +0530

    Added JS-PRACTICE-QUESTIONS

---
+++ IF-ELSE/EASY/app.js
@@ -0,0 +1,157 @@
+// 1. Take a number and print whether it’s positive, negative, or zero.
+// --------------------------------------------------------------------
+
+// let num = 10;
+
+// if(num < 0){
+//     console.log(`number : ${num} is negative`)
+// }
+// else if(num > 0){
+//     console.log(`number : ${num} is positive`)
+// }
+// else{
+//     console.log(`number : ${num} is zero`)
+// }
+
+// -----------------------------------------------------
+
+// 2. Check if a number is even or odd.
+// ---------------------------------------
+
+// let num = 10;
+
+// if(num %2 == 0){
+//     console.log(`number : ${num} is even`)
+// }
+// else{
+//     console.log(`number : ${num} is odd`)
+// }
+
+// -----------------------------------------------
+
+// 3. Check if a number is divisible by 5.
+// -----------------------------------------
+
+// let num = 10;
+
+// if(num % 5 == 0){
+//     console.log(`number : ${num} is divisible by 5`)
+// }
+// else{
+//     console.log(`number : ${num} is not divisible by 5`)
+// }
+
+// ------------------------------------------------------------
+
+//  4. Check if a number is divisible by both 3 and 5.
+// -----------------------------------------------------
+
+// let num = 10;
+
+// if(num % 3 == 0 && num % 5 == 0){
+//     console.log(`number : ${num} is divisible by both 3 and 5`);
+// }
+// else{
+//     console.log(`number : ${num} is not divisible by both 3 and 5`);
+// }
+
+// --------------------------------------------------------------------------
+
+// 5. Check if a given year is a leap year.
+// ------------------------------------------
+
+// let year = 2020;
+
+// if((year % 4 == 0 && year % 100 !== 0) || year % 400 == 0){
+//     console.log(`year : ${year} is a leap year`)
+// }
+// else{
+//     console.log(`year : ${year} is not a leap year`)
+// }
+
+// --------------------------------------------------------------
+
+// 6. Take two numbers and print the larger one.
+// ----------------------------------------------
+
+// let  n1 = 10;
+// let n2 = 20;
+
+// if(n1 > n2){
+//     console.log(`n1 : ${n1} is larger one`)
+// }
+// else{
+//     console.log(`n2 : ${n2} is larger one`)
+// }
+
+// ------------------------------------------------
+
+// 7. Take three numbers and print the largest.
+// ---------------------------------------------
+
+// let  n1 = 10;
+// let n2 = 20;
+// let n3 = 30;
+
+// if(n1 > n2 && n1 > n3){
+//     console.log(`n1 : ${n1} is largest`)
+// }
+// else if(n2 > n3 && n2 > n1){
+//     console.log(`n2 : ${n2} is largest`)
+// }
+// else{
+//     console.log(`n3 : ${n3} is largest`)
+// }
+
+// ----------------------------------------------------
+
+// 8. Take a temperature value and print “Cold”, “Warm”, or “Hot” using range conditions.
+// -----------------------------------------------------------------------------------------
+
+// let temp = -10;
+
+// if(temp >= 30){
+//     console.log(`Temp : ${temp} is too hot`)
+// }
+// else if(temp >= 15 && temp < 30){
+//     console.log(`Temp : ${temp} is warm`)
+// }
+// else{
+//     console.log(`Temp : ${temp} is cold`)
+// }
+
+// ------------------------------------------------
+
+// 9. Take a character and check if it’s a vowel or consonant.
+// -------------------------------------------------------------
+
+// let ch = "S"
+
+// if(ch == "a" || ch == "A" || ch == "e" || ch == "E" || ch == "i" || ch == "I" || ch == "o" || ch == "O" || ch == "u" || ch == "U"){
+//     console.log(`ch : ${ch} is vowel`)
+// }
+// else{
+//     console.log(`ch : ${ch} is consonant`)
+// }
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+// 10. Take a character and check whether it’s uppercase, lowercase, a digit, or a special character.
+// ----------------------------------------------------------------------------------------------------
+
+// let ch = "S";
+
+// if(ch >= "A" && ch <= "Z"){
+//     console.log(`ch : ${ch} is in Uppercase`)
+// }
+// else if(ch >= "a" && ch <= "z"){
+//     console.log(`ch : ${ch} is in lowercase`)
+// }
+// else if(ch >= 1 && ch <= 9){
+//     console.log(`ch : ${ch} is in digit`)
+// }
+// else{
+//     console.log(`ch : ${ch} is in special character`)
+// }
+
+// ------------------------------------------------------------------------------------------------------
