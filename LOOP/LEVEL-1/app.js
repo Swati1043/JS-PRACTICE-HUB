7aae7dd549582028ee934961614bb1685c2d029d
Swati <pandeyswati1043@gmail.com>
2026-06-17 17:44:13 +0530

    Add LOOP-LEVEL-1 Questions

---
+++ LOOP/LEVEL-1/app.js
@@ -0,0 +1,124 @@
+// 1. Print numbers from 1 to 10.
+// ------------------------------
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(i);
+// }
+
+// -----------------------------------
+
+// 2. Print all even numbers between 1 and 100.
+// ----------------------------------------------
+
+// for(let i = 1 ; i <= 100 ; i++){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
+
+// -------------------------------------------------
+
+// 3. Print all odd numbers between 1 and 100.
+// ----------------------------------------------
+
+// for(let i = 1 ; i <= 100 ; i++){
+//     if(i % 2 != 0){
+//         console.log(i);
+//     }
+// }
+
+// ------------------------------------------------
+
+// 4. Print numbers from 10 down to 1.
+// ------------------------------------
+
+// for(let i = 10 ; i >= 1 ; i--){
+//     console.log(i);
+// }
+
+// -----------------------------------------
+
+// 5. Print the table of a given number (n × 1 to n × 10).
+// --------------------------------------------------------
+
+// let num = 7;
+
+// for(let i = 1 ; i <= 10 ; i++){
+//     console.log(`${num} * ${i} = ${num * i}`)
+// }
+
+// --------------------------------------------------------
+
+// 6. Print the sum of first n natural numbers.
+// ---------------------------------------------
+
+// let n = 5;
+// let sum = 0;
+
+// for(let i = 1 ; i <= n ; i++)
+// {
+//     sum = sum + i;
+// }
+// console.log(`Sum of first n natural numbers : ${sum}`)
+
+// -------------------------------------------------------------
+
+// 7. Print the sum of all even numbers up to n.
+// ----------------------------------------------
+
+// let n = 100;
+// let sum = 0;
+
+// for(let i = 1 ; i <= n ; i++){
+//     if(i % 2 == 0){
+//         sum = sum + i;
+//     }
+// }
+// console.log(sum);
+
+// -------------------------------------------------
+
+// 8. Print the sum of all odd numbers up to n.
+// ---------------------------------------------
+
+// let n = 100;
+// let sum = 0;
+
+// for(let i = 1 ; i <= n ; i++){
+//     if(i % 2 != 0){
+//         sum = sum + i;
+//     }
+// }
+// console.log(sum);
+
+// --------------------------------------------------
+
+// 9. Print the factorial of a given number.
+// ------------------------------------------
+
+// let num = 5;
+// let fact = 1;
+
+// for(let i = 1 ; i <= num ; i++){
+//     fact = fact * i;
+// }
+// console.log(fact);
+
+// ---------------------------------------
+
+// 10. Print the product of digits of a given number.
+// ---------------------------------------------------
+
+// let num = 1234;
+// let prod = 1;
+
+// while(num > 0){
+//     let digit = num % 10;
+
+//     prod *= digit;
+
+//     num = Math.floor(num / 10);
+// }
+// console.log(prod);
+
+// -----------------------------------
